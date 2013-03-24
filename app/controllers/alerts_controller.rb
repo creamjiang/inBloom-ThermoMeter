eacf5a617c287765861eb79c2f3b4b4230c9f475
Sushant Mohanty <smohanty@mohantyfamily.org>
2013-03-24 11:25:00 -0400

    alert text and matts number

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,11 +1,11 @@
 class AlertsController < ApplicationController
   def sendit
     name = params[:name] || 'John Smith'
-    text = params[:text] || "Alert for #{name}"
+    text = params[:text] || "#{name} is in danger of failing 8th Gr English for the year. Please contact Mrs. Kim ASAP to arrange a conference."
     phone = params[:phone] || '+12025551212'
     IbTwilio.send(phone, text)
 
     flash[:notice] = "Alert sent for #{name}"
-    redirect_to klasses_url && return
+    redirect_to :back and return
   end
 end
