b1d0f7fc84fb18ae3b60a0d119e8f7f008927803
Sushant Mohanty <smohanty@mohantyfamily.org>
2013-03-23 22:25:17 -0400

    get credential config file optional

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,14 @@ app_config_file = Rails.root.join('config/config.yml')
 CONFIG_YML = YAML.load_file(app_config_file)
 APP_CONFIG = CONFIG_YML[Rails.env]
 
+# load credentials
+CREDENTIAL_CONFIG = {}
+credential_config = Rails.root.join('.credentials.yml')
+if File.exist?(credential_config)
+	my_yml = YAML.load_file(credential_config)
+	CREDENTIAL_CONFIG.merge!(my_yml[Rails.env])
+end
+debugger
+
 # Initialize the rails application
 SlcSample2::Application.initialize!
