d752da1d7fec3dacae56b50484f292bf2e3611ba
Sushant Mohanty <smohanty@mohantyfamily.org>
2013-03-23 22:29:44 -0400

    nc

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,27 @@ app_config_file = Rails.root.join('config/config.yml')
 CONFIG_YML = YAML.load_file(app_config_file)
 APP_CONFIG = CONFIG_YML[Rails.env]
 
-# load credentials
+# load credentials if present - you may want to put these for production config outside of git
+# example .credentials.yml file below
+# defaults: &defaults
+#   INBLOOM_KEY: your_key
+#   INBLOOM_SECRET: your_secret
+
+# development: &development
+#   <<: *defaults
+
+# integration:
+#   <<: *defaults
+
+# production:
+#   <<: *defaults
+#
 CREDENTIAL_CONFIG = {}
 credential_config = Rails.root.join('.credentials.yml')
 if File.exist?(credential_config)
 	my_yml = YAML.load_file(credential_config)
 	CREDENTIAL_CONFIG.merge!(my_yml[Rails.env])
 end
-debugger
 
 # Initialize the rails application
 SlcSample2::Application.initialize!
