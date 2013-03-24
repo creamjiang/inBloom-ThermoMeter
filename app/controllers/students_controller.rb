936f681acda966f99ba47cbc025caa9b2046b9ec
Sushant Mohanty <smohanty@mohantyfamily.org>
2013-03-23 21:26:46 -0400

    hidden credentials

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,13 @@
 class StudentsController < ApplicationController
   
   def index
-    def slc_request
-      if current_user
-        path = 'https://api.sandbox.slcedu.org/api/rest/v1/students'
-        headers = { "Accept" => 'application/vnd.slc+json',
-                      "Content-Type" => 'application/vnd.slc+json',
-                      "Authorization" => "bearer #{session[:token]}" }
+    if current_user
+      path = 'https://api.sandbox.slcedu.org/api/rest/v1/students'
+      headers = { "Accept" => 'application/vnd.slc+json',
+                    "Content-Type" => 'application/vnd.slc+json',
+                    "Authorization" => "bearer #{session[:token]}" }
 
-        @slc_request = HTTParty.send(request.method.underscore.to_sym, path, :headers => headers)
-      end
+      @slc_request = HTTParty.send(request.method.underscore.to_sym, path, :headers => headers)
     end
   end
   
