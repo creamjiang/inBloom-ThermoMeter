ff009815c40ba284a6743c88e15e5b8943594e83
Matt Scilipoti <matt@scilipoti.name>
2013-03-26 09:18:44 -0400

    mock_in_bloom: add config to initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slc, get_env('INBLOOM_KEY'), get_env('INBLOOM_SECRET'), :setup => lambda { |env| 
-     env['omniauth.strategy'].options[:client_options].site = 'https://api.sandbox.slcedu.org'
-  }
-  raise "InBloom Sandbox Key or Secret missing" unless get_env('INBLOOM_KEY') && get_env('INBLOOM_SECRET')
+unless APP_CONFIG['mock_in_bloom']
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :slc, get_env('INBLOOM_KEY'), get_env('INBLOOM_SECRET'), :setup => lambda { |env|
+      env['omniauth.strategy'].options[:client_options].site = 'https://api.sandbox.slcedu.org'
+    }
+    raise "InBloom Sandbox Key or Secret missing" unless get_env('INBLOOM_KEY') && get_env('INBLOOM_SECRET')
+  end
 end
 
 # override default behavior - bit of monkey patch to allow testing in localhost
@@ -17,8 +19,8 @@ module OmniAuth
   end
 end
 
-private 
+private
 
 def get_env(key)
-	CREDENTIAL_CONFIG[key].present? ? CREDENTIAL_CONFIG[key] : ENV[key]
+  CREDENTIAL_CONFIG[key].present? ? CREDENTIAL_CONFIG[key] : ENV[key]
 end
